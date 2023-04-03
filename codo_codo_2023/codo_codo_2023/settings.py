9819c05c4922c6f558561398e7deb19f89770985
maru33luc <maru33luc@gmail.com>
2023-04-02 22:55:07 -0300

    resuelto temakey

--- codo_codo_2023/codo_codo_2023/settings.py
+++ codo_codo_2023/codo_codo_2023/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
